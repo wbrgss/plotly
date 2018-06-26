cb0d97d7c4668d9e3a5d9e22d4718e90b8e8267d
wbrgss <wbrgss@gmail.com>
2018-06-26 04:29:36 -0400

    app.py --> map.py

--- map.py
+++ map.py
@@ -133,4 +133,4 @@ def update_graph(n, colorscale, main_graph_layout):
     return fig
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server(debug=True,host='0.0.0.0',port=80)
